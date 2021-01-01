83bcd2ae25c2e94cf0f80264bb56baac27b352df
guilherme <gsfl3101@gmail.com>
2021-01-01 18:30:59 -0300

    Challenge solution

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,67 @@ app.use(cors());
 
 const repositories = [];
 
+function validateUuid(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) return response.status(400).json({ error: "Invalid id"});
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateUuid, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIdx = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIdx < 0 ) return response.status(400).send();
+
+  repositories[repoIdx] = {
+    id,
+    title,
+    url,
+    techs, likes: repositories[repoIdx].likes
+  };
+
+  return response.json(repositories[repoIdx]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateUuid, (request, response) => {
+  const { id } = request.params;
+  const repoIdx = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIdx < 0 ) return response.status(400).send();
+
+  repositories.splice(repoIdx, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateUuid, (request, response) => {
+  const { id } = request.params;
+
+  const repoIdx = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIdx < 0 ) return response.status(400).send();
+
+  repositories[repoIdx].likes++;
+
+  return response.json(repositories[repoIdx]);
 });
 
 module.exports = app;
